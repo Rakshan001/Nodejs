ba07f2e6fd9fd5b36a7f8dc40f2be0bb9568aad3
Rakshan001 <rakshanshetty2003@gmail.com>
2025-06-25 23:14:23 +0530

    added the register file with tailwind and flowbite cdn

--- Projects/DRIVE/routes/user.routes.js
+++ Projects/DRIVE/routes/user.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
 router.get('/test',(req,res)=>{
-    res.send("hello from router")
+    // res.send("hello from router")
+    res.render('register')
 })
 
 module.exports = router;
