e7a097f4e681be2adcf7e2838c085922f6571703
Rakshan001 <rakshanshetty2003@gmail.com>
2025-06-26 22:45:34 +0530

    `Added dotenv, express-validator, and updated routes and views for user registration`

--- Projects/DRIVE/app.js
+++ Projects/DRIVE/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const userRouter = require('./routes/user.routes')
+const dotenv = require('dotenv');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.set("view engine","ejs")
 
 
